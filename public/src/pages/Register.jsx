13f7be3440136679181e72897f46d6e74a8b3ad1
Ali <aliraza.12748g@gmail.com>
2022-10-01 20:25:51 +0500

    resgister and login authentication completed

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,35 +1,96 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react'
+import { Link ,useNavigate} from 'react-router-dom';
 import styled from 'styled-components'
 import Logo from "../assets/logo.svg"
+import {ToastContainer,toast}from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css"
+import axios from 'axios'
+import { registerRoute } from '../utils/ApiRoutes';
 const Register = () => {
-  const handleSubmit = (event) => {
+  const nevigate=useNavigate()
+  const [values,setValues]=useState({
+    username:"",
+    email:"",
+    password:"",
+    confirmPassword:""
+  })
+  const handleSubmit = async(event) => {
     event.preventDefault();
-    alert("Form")
+    if(handleValidation()){
+      console.log("in valid api register",registerRoute)
+      const {password,username,email}=values;
+        const {data}=await axios.post(registerRoute,{
+          username,
+          email,
+          password,
+        });
+        if(data.status===false){
+          toast.error(data.msg,toastOptions)
+        }
+        if(data.status===true){
+          localStorage.setItem("chat-app-user",JSON.stringify(data.user))
+          nevigate('/')
+        }
+    }
   }
-  const handleChange = (e) => {
+  const toastOptions={
+    position:"bottom-right",
+    autoClose:8000,
+    pauseOnHover:true,
+    draggable:true,
+    theme:"dark"
+  }
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+  const handleValidation = () => {
+    const { password, confirmPassword, username, email } = values;
+    if (password !== confirmPassword) {
+      toast.error(
+        "Password and confirm password should be same.",
+        toastOptions
+      );
+      return false;
+    } else if (username.length < 3) {
+      toast.error(
+        "Username should be greater than 3 characters.",
+        toastOptions
+      );
+      return false;
+    } else if (password.length < 8) {
+      toast.error(
+        "Password should be equal or greater than 8 characters.",
+        toastOptions
+      );
+      return false;
+    } else if (email === "") {
+      toast.error("Email is required.", toastOptions);
+      return false;
+    }
 
-  }
+    return true;
+  };
+
+ 
   return (
     <>
       <FormContainer>
         <form onSubmit={(event) => handleSubmit(event)}>
-
           <div className="brand">
             <img src={Logo} alt="Logo" />
             <h1>
-              snappy
+              Register
             </h1>
-
           </div>
           <input type="text" placeholder='Username' name="username" onChange={e => handleChange(e)} />
           <input type="email" placeholder='Email' name="email" onChange={e => handleChange(e)} />
           <input type="password" placeholder='Password' name="password" onChange={e => handleChange(e)} />
-          <input type="password" placeholder='Confirm Password' name="confirmPasword" onChange={e => handleChange(e)} />
+          <input type="password" placeholder='Confirm Password' name="confirmPassword" onChange={e => handleChange(e)} />
           <button type='submit'>Create User</button>
           <span>Already have account? <Link to="/login">Login</Link></span>
         </form>
       </FormContainer>
+      <ToastContainer/>
     </>
   )
 }
--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 const express=require("express")
 const cors =require("cors");
 const mongoose=require("mongoose")
+const userRoutes =require('./routes/userRoutes') 
 
 const app =express();
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+
+// api calling routes
+app.use('/api/auth/',userRoutes)
 
 // database Con
 mongoose.connect(process.env.MONGO_URL).then((res)=>{
